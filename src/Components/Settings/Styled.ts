3081a867b0ccdc8cc52ad8a7b992c6b5c1ac7a62
Patrik Marin <patrik.marin@gmail.com>
2019-09-25 14:54:00 +0300

    Add hamburger menu

--- src/Components/Settings/Styled.ts
+++ src/Components/Settings/Styled.ts
@@ -9,16 +9,19 @@ const getStylePerProps = (
 ): {
   maxWidth: undefined | React.CSSProperties['maxWidth'];
   width: undefined | React.CSSProperties['width'];
+  hamburgerTop: React.CSSProperties['height'];
 } => {
   if (props.open) {
     return {
       maxWidth: '300px',
       width: '100%',
+      hamburgerTop: 0,
     };
   }
   return {
     maxWidth: undefined,
     width: undefined,
+    hamburgerTop: '2rem',
   };
 };
 
@@ -27,17 +30,41 @@ const Container = styled.div<ContainerProps>`
   box-shadow: ${props => props.theme.boxShadow};
   padding: 2rem;
   height: 100%;
+  text-transform: uppercase;
   box-sizing: border-box;
   h1 {
+    font-weight: bold;
     margin-bottom: 1rem;
   }
-  h2 {
+  h3 {
     margin-bottom: 0.5rem;
   }
   width: ${props => getStylePerProps(props).width};
   max-width: ${props => getStylePerProps(props).maxWidth};
+
+  .hamburger {
+    padding: 0;
+  }
+
+  .hamburger-box {
+    height: ${props => getStylePerProps(props).hamburgerTop};
+  }
+
+  .hamburger-inner,
+  .hamburger-inner::before,
+  .hamburger-inner::after {
+    width: 2rem;
+    height: 4px;
+  }
+`;
+
+const HeaderBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export default {
   Container,
+  HeaderBar,
 };
--- src/Components/Settings/index.tsx
+++ src/Components/Settings/index.tsx
@@ -7,16 +7,43 @@ interface Props {
   selectTheme: (themeName: APP_THEME) => void;
 }
 
+const HamburgerMenu = ({
+  toggle,
+  open,
+}: {
+  toggle: () => void;
+  open: boolean;
+}) => {
+  const extraClassName = open ? ['is-active'] : [];
+  return (
+    <button
+      onClick={() => toggle()}
+      className={['hamburger', 'hamburger--collapse']
+        .concat(extraClassName)
+        .join(' ')}
+      type="button"
+      aria-label="Menu"
+      aria-controls="navigation"
+      aria-expanded={open}
+    >
+      <span className="hamburger-box">
+        <span className="hamburger-inner"></span>
+      </span>
+    </button>
+  );
+};
+
 const Settings = (props: Props) => {
   const [open, setOpen] = React.useState(false);
   return (
     <Styled.Container open={open}>
-      {!open ? (
-        <h1 onClick={() => setOpen(true)}>O</h1>
-      ) : (
+      <Styled.HeaderBar>
+        {open && <h1>Settings</h1>}
+        <HamburgerMenu toggle={() => setOpen(!open)} open={open} />
+      </Styled.HeaderBar>
+      {open && (
         <React.Fragment>
-          <h1 onClick={() => setOpen(false)}>Settings</h1>
-          <h2>Select theme</h2>
+          <h3>Select theme</h3>
           {APP_THEME_NAMES.map(themeName => (
             <button
               key={themeName}
