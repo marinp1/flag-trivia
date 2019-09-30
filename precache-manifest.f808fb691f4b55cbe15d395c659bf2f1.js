9d78496fb5093a0163082c2f1f0ad896aba9e347
marinp1 <marinp1@users.noreply.github.com>
2019-09-30 15:24:57 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.f808fb691f4b55cbe15d395c659bf2f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b5ecb8d8bc010452aec053bcacb23b",
+    "url": "/flag-trivia/index.html"
+  },
+  {
+    "revision": "2ff31f7495da6ba86900",
+    "url": "/flag-trivia/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "c72c21c45ecb5d94c4ff",
+    "url": "/flag-trivia/static/css/main.dd3409e9.chunk.css"
+  },
+  {
+    "revision": "2ff31f7495da6ba86900",
+    "url": "/flag-trivia/static/js/2.4bc0553b.chunk.js"
+  },
+  {
+    "revision": "1078b38a0642bb260597",
+    "url": "/flag-trivia/static/js/3.3cf885f9.chunk.js"
+  },
+  {
+    "revision": "c72c21c45ecb5d94c4ff",
+    "url": "/flag-trivia/static/js/main.56d94433.chunk.js"
+  },
+  {
+    "revision": "e269b82cbfadedc7e610",
+    "url": "/flag-trivia/static/js/runtime-main.d2f1f3fe.js"
+  }
+]);
